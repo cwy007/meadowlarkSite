212935b26ddebb889a75650ab12760ae411fe9b4
chenweiyan <chanweiyan007@gmail.com>
2023-01-17 16:28:33 +0800

    feat: dynamic view

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-// import { engine } from 'express-handlebars';
+
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
 
 const app = express()
 
@@ -15,7 +22,10 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
 
 app.use((req, res) => {
   res.status(404)
