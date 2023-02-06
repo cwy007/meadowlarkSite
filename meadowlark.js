a526b2c113ed96d622f348f2155d39c220454e01
chenweiyan <chanweiyan007@gmail.com>
2023-02-06 22:24:36 +0800

    fix: calls[0][0] -> 哪次调用，哪个参数

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -13,22 +13,15 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+// 404
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+// 500
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; ` +
